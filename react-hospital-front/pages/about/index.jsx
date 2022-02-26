7a2af8c42a08d047356bd798a887e4cc04fda56b
mukhulaazam <mukhulaazam@gmail.com>
2022-02-26 23:53:07 +0600

    remove lumen project and replace with laravel

--- react-hospital-front/pages/about/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Home() {
-    let router = useRouter()
-
-    return (
-        <h4>{router.pathname}</h4>
-    )
-}
--- react-hospital-front/pages/contact/index.jsx
+++
@@ -1,7 +0,0 @@
-import Router, { useRouter } from "next/router";
-
-export default function Home() {
-    let rotuer = useRouter()
-
-    return <div>{ rotuer.pathname }</div>
-}
